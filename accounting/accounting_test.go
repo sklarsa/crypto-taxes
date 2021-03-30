e5e95e9db4b035836d8aed6e98001828f6244559
Steven Sklar <sklarsa@gmail.com>
2021-03-30 18:51:20 -0400

    Simplifies testing, sort of

--- accounting/accounting_test.go
+++ accounting/accounting_test.go
@@ -11,8 +11,8 @@ import (
 // TestBasicLotHistoryUsage performs basic functionality tests
 // for the LotHistory struct.  Similar to a smoketest.
 func TestBasicLotHistoryUsage(t *testing.T) {
-	jan1st := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
-	feb1st := time.Date(2020, 2, 1, 0, 0, 0, 0, time.UTC)
+	t0 := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
+	asset := "BTC"
 
 	h := &LotHistory{
 		Asset: "BTC",
@@ -21,99 +21,122 @@ func TestBasicLotHistoryUsage(t *testing.T) {
 
 	assert.Empty(t, h.Lots)
 
-	// BUY 100 BTC @ $1
-	h.Buy(&Lot{
-		PurchaseDate: jan1st,
-		Quantity:     decimal.NewFromInt(100),
-		Spot:         decimal.NewFromInt(1),
-	})
-
-	assert.Equal(t, 1, len(h.Lots))
-	assert.Equal(t, decimal.NewFromInt(100), h.TotalCost())
-
-	// BUY 100 BTC @ $2
-	h.Buy(&Lot{
-		PurchaseDate: jan1st,
-		Quantity:     decimal.NewFromInt(100),
-		Spot:         decimal.NewFromInt(2),
-	})
-
-	assert.Equal(t, 2, len(h.Lots))
-	assert.Equal(t, decimal.NewFromInt(300), h.TotalCost())
-
-	// SELL 200 BTC @ $3
-	sales := make(chan *Sale)
-	taxableSales := make([]*Sale, 0)
-
-	go func() {
-		h.Sell(decimal.NewFromInt(200), decimal.NewFromInt(3), feb1st, sales)
-		defer close(sales)
-	}()
-
-	for s := range sales {
-		taxableSales = append(taxableSales, s)
+	transactions := []Transaction{
+		// BUY 100 BTC @ $1
+		{
+			Timestamp: t0,
+			Action:    BUY,
+			Asset:     asset,
+			Quantity:  decimal.NewFromInt(100),
+			Spot:      decimal.NewFromInt(1),
+		},
+		// BUY 100 BTC @ $2
+		{
+			Timestamp: t0.AddDate(0, 0, 1),
+			Action:    BUY,
+			Asset:     asset,
+			Quantity:  decimal.NewFromInt(100),
+			Spot:      decimal.NewFromInt(2),
+		},
+		// SELL 200 BTC @ 3
+		{
+			Timestamp: t0.AddDate(0, 0, 2),
+			Action:    SELL,
+			Asset:     asset,
+			Quantity:  decimal.NewFromInt(200),
+			Spot:      decimal.NewFromInt(3),
+		},
+		// BUY 100 BTC @ $10
+		{
+			Timestamp: t0.AddDate(0, 0, 3),
+			Action:    BUY,
+			Asset:     asset,
+			Quantity:  decimal.NewFromInt(100),
+			Spot:      decimal.NewFromInt(10),
+		},
+		// BUY 100 BTC @ $5
+		{
+			Timestamp: t0.AddDate(0, 0, 4),
+			Action:    BUY,
+			Asset:     asset,
+			Quantity:  decimal.NewFromInt(100),
+			Spot:      decimal.NewFromInt(5),
+		},
+		// SELL 5 BTC @ 2
+		{
+			Timestamp: t0.AddDate(0, 0, 5),
+			Action:    SELL,
+			Asset:     asset,
+			Quantity:  decimal.NewFromInt(5),
+			Spot:      decimal.NewFromInt(2),
+		},
+		// SELL 94 BTC @ 100
+		{
+			Timestamp: t0.AddDate(0, 0, 6),
+			Action:    SELL,
+			Asset:     asset,
+			Quantity:  decimal.NewFromInt(94),
+			Spot:      decimal.NewFromInt(100),
+		},
+		// SELL 2 BTC @ 5
+		{
+			Timestamp: t0.AddDate(0, 0, 7),
+			Action:    SELL,
+			Asset:     asset,
+			Quantity:  decimal.NewFromInt(2),
+			Spot:      decimal.NewFromInt(5),
+		},
 	}
 
-	assert.Equal(t, 2, len(taxableSales))
-	firstSale := taxableSales[0]
-	assert.Equal(t, decimal.NewFromInt(100), firstSale.Quantity)
-	assert.Equal(t, decimal.NewFromInt(1*100), firstSale.FifoCost)
-	assert.Equal(t, jan1st, firstSale.PurchaseDate)
-	assert.Equal(t, feb1st, firstSale.SaleDate)
-
-	// BUY 100 BTC @ 10
-	h.Buy(&Lot{
-		PurchaseDate: jan1st,
-		Quantity:     decimal.NewFromInt(100),
-		Spot:         decimal.NewFromInt(10),
-	})
-
-	// BUY 100 BTC @ 5
-	h.Buy(&Lot{
-		PurchaseDate: jan1st,
-		Quantity:     decimal.NewFromInt(100),
-		Spot:         decimal.NewFromInt(5),
-	})
-
-	sales = make(chan *Sale)
-	taxableSales = make([]*Sale, 0)
-
+	sales := make(chan *Sale)
 	go func() {
 		defer close(sales)
-		// SELL 5 BTC @ 2
-		h.Sell(decimal.NewFromInt(5), decimal.NewFromInt(2), feb1st, sales)
-
-		// SELL 94 BTC @ 100
-		h.Sell(decimal.NewFromInt(94), decimal.NewFromInt(100), feb1st, sales)
-
-		// SELL 2 BTC @ 5
-		h.Sell(decimal.NewFromInt(2), decimal.NewFromInt(5), feb1st, sales)
-
+		for _, t := range transactions {
+			switch t.Action {
+			case BUY:
+				h.Buy(t.ToLot())
+			case SELL:
+				h.Sell(t.Quantity, t.Spot, t.Timestamp, sales)
+			}
+		}
 	}()
 
-	salesCtr := 0
+	ctr := 0
 	for s := range sales {
-		switch salesCtr {
+		switch ctr {
 		case 0:
+			// Sale #1 (200@3) Lot #1 100@1
+			assert.Equal(t, decimal.NewFromInt(100*1), s.FifoCost)
+			assert.Equal(t, decimal.NewFromInt(100*3), s.Proceeds)
+		case 1:
+			// Sale #1 (200@3) Lot #2 100@2
+			assert.Equal(t, decimal.NewFromInt(100*2), s.FifoCost)
+			assert.Equal(t, decimal.NewFromInt(100*3), s.Proceeds)
+		case 2:
+			// Sale #2 (5@2) Lot #1 100@10
 			assert.Equal(t, decimal.NewFromInt(5*10), s.FifoCost)
 			assert.Equal(t, decimal.NewFromInt(5*2), s.Proceeds)
-		case 1:
+		case 3:
+			// Sale #3 (94@100) Lot #1 95@10
 			assert.Equal(t, decimal.NewFromInt(94*10), s.FifoCost)
 			assert.Equal(t, decimal.NewFromInt(94*100), s.Proceeds)
-		case 2:
+		case 4:
+			// Sale #4 (2@5) Lot #1 1@10
 			assert.Equal(t, decimal.NewFromInt(1*10), s.FifoCost)
 			assert.Equal(t, decimal.NewFromInt(1*5), s.Proceeds)
-		case 3:
+		case 5:
+			// Sale #4 (2@5) Lot #2 1@5
 			assert.Equal(t, decimal.NewFromInt(1*5), s.FifoCost)
 			assert.Equal(t, decimal.NewFromInt(1*5), s.Proceeds)
-		default:
-			t.Error("Too many sales")
-		}
 
-		salesCtr++
+		default:
+			t.Errorf("Too many sales!")
+		}
+		ctr++
 	}
 
-	assert.Equal(t, 4, salesCtr)
+	assert.Equal(t, 1, len(h.Lots))
+	assert.Equal(t, decimal.NewFromInt(99), h.Quantity())
 }
 
 func TestLotHistoryEdgeCases(t *testing.T) {
